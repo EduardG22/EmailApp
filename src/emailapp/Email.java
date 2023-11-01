2c0eae7759a4ebea6371fd80ffb0e61cf9fc1f48
EduardG22 <popaeduard22@gmail.com>
2023-11-01 15:45:15 +0100

    Finish the application

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -9,7 +9,7 @@ public class Email {
     private String password;
     private String department;
     private String email;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
     private String companySuffix = "company.com";
@@ -19,11 +19,11 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
 
         // Call a method asking for the department
         this.department = setDepartment();
-        System.out.println("Department: " + this.department);
+
 
         // Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
@@ -31,7 +31,7 @@ public class Email {
 
         // Combine elements to generate email
         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-        System.out.println("Your email is: " + email);
+
 
     }
 
@@ -63,8 +63,33 @@ public class Email {
     }
 
     // Set the mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
 
     // Set the alternate email
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
 
     // Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "DISPLAY NAME: " + firstName + " " + lastName +
+                "\nCOMPANY EMAIL: " + email +
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    }
 }
